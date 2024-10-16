5ef3f1f5df8860fc43d0bc7c12ce1fe040b62a5f
RoPeak <ronanpeacock2004@gmail.com>
2024-10-16 20:55:28 +0100

    Completion

--- src/Snake.hs
+++ src/Snake.hs
@@ -1,60 +1,153 @@
-module Snake where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Snake
+  ( initGame
+  , step
+  , turn
+  , Game(..)
+  , Direction(..)
+  , dead, food, score, snake
+  , height, width
+  ) where
 
--- Imports --
 import Control.Applicative ((<|>))
-import Control.Monad (guard)
+import Control.Monad (guard, void)
 import Data.Maybe (fromMaybe)
 
-import Data.Sequence (Seq, ViewL(...), ViewR(..), (<|))
+import Control.Lens hiding ((<|), (|>), (:>), (:<), index)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.State.Class (MonadState, modify, get)
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.State (execState)
+import Control.Monad.Extra (orM, unlessM)
+import Data.Sequence (Seq(..), (<|), index)
 import qualified Data.Sequence as S
-import Lens.Micro.TH (makeLenses)
-import Lens.Micro ((&), (.~), (%~), (^.))
 import Linear.V2 (V2(..), _x, _y)
 import System.Random (Random(..), newStdGen)
 
+-- Types
 
--- Types --
 data Game = Game
-    { _snake  :: Snake,         -- Snake as a sequence of points
-      _dir    :: Direction,     -- Direction of snake
-      _food   :: Coord,         -- Location of the food
-      _foods  :: Stream Coord,  -- Infinite list of food locations  
-      _dead   :: Bool,          -- Game over flag
-      _paused :: Bool,          -- Paused flag
-      _score  :: Int,           -- Player score
-      _frozen :: Bool           -- Freeze to disallow duplicate turns
-    } deriving (Show)
+  { _snake  :: Snake        -- ^ snake as a sequence of points in N2
+  , _dir    :: Direction    -- ^ direction
+  , _food   :: Coord        -- ^ location of the food
+  , _foods  :: Stream Coord -- ^ infinite list of random next food locations
+  , _dead   :: Bool         -- ^ game over flag
+  , _paused :: Bool         -- ^ paused flag
+  , _score  :: Int          -- ^ score
+  , _locked :: Bool         -- ^ lock to disallow duplicate turns between time steps
+  } deriving (Show)
 
 type Coord = V2 Int
+
 type Snake = Seq Coord
 
-data Stream a = a :| Stream as
-    deriving (Show)
+data Stream a = a :| Stream a
+  deriving (Show)
 
 data Direction
-    = North | East | South | West
-    deriving (Eq, Show)
+  = North
+  | South
+  | East
+  | West
+  deriving (Eq, Show)
 
+makeLenses ''Game
 
--- Core functions --
--- Step forward in time
-step :: Game -> Game
-step g = fromMaybe g $ do
-    guard (not $ g ^. paused || g ^. dead)
-    let g' = g & frozen .~ False
-    return . fromMaybe (move g') $ die g' <|> eatFood g'
+-- Constants
 
--- Possibly die if next head position is disallowed
-die :: Game -> Maybe Game
+height, width :: Int
+height = 20
+width = 20
 
--- Possibly eat food if next head position is food
-eatFood :: Game -> Maybe Game
+-- Functions
 
--- Move snake along 
-move :: Game -> Game
+-- | Step forward in time
+step :: MonadState Game m => m ()
+step = void . runMaybeT $ do
 
--- Turn game direction (orthogonally)
-turn :: Direction -> Game -> Game
+  -- Make sure the game isn't paused or over
+  MaybeT $ guard . not <$> orM [use paused, use dead]
 
--- Initialise a paused game with random food location
-initGame :: IO Game
+  -- Unlock from last directional turn
+  MaybeT . fmap Just $ locked .= False
+
+  -- die (moved into self), eat (moved into food), or move (move into space)
+  die <|> eatFood <|> MaybeT (Just <$> move)
+
+-- | Possibly die if next head position is in snake
+die :: MonadState Game m => MaybeT m ()
+die = do
+  MaybeT . fmap guard $ elem <$> nextHead <*> (use snake)
+  MaybeT . fmap Just $ dead .= True
+
+-- | Possibly eat food if next head position is food
+eatFood :: MonadState Game m => MaybeT m ()
+eatFood = do
+  MaybeT . fmap guard $ (==) <$> nextHead <*> (use food)
+  MaybeT . fmap Just $ do
+    modifying score (+ 10)
+    nh <- nextHead
+    modifying snake (nh <|)
+    nextFood
+
+-- | Set a valid next food coordinate
+nextFood :: MonadState Game m => m ()
+nextFood = do
+  (f :| fs) <- use foods
+  foods .= fs
+  elem f <$> use snake >>= \case
+    True -> nextFood
+    False -> food .= f
+
+-- | Move snake along in a marquee fashion
+move :: MonadState Game m => m ()
+move = do
+  nh <- nextHead
+  modifying snake $ \(s :|> _) -> nh <| s
+
+-- | Get next head position of the snake
+nextHead :: MonadState Game m => m Coord
+nextHead = get <&> \(Game {_snake = (a :<| _), _dir = d}) -> case d of
+  North -> a & _y %~ (\y -> (y + 1) `mod` height)
+  South -> a & _y %~ (\y -> (y - 1) `mod` height)
+  East  -> a & _x %~ (\x -> (x + 1) `mod` width)
+  West  -> a & _x %~ (\x -> (x - 1) `mod` width)
+
+-- | Turn game direction (only turns orthogonally)
+--
+-- Implicitly unpauses yet locks game
+turn :: MonadState Game m => Direction -> m ()
+turn d = do
+  unlessM (use locked) $ do
+    modifying dir (turnDir d)
+    assign paused False
+    assign locked True
+
+turnDir :: Direction -> Direction -> Direction
+turnDir n c | c `elem` [North, South] && n `elem` [East, West] = n
+            | c `elem` [East, West] && n `elem` [North, South] = n
+            | otherwise = c
+
+-- | Initialize a paused game with random food location
+initGame :: MonadIO m => m Game
+initGame = do
+  (f :| fs) <-
+    fromList . randomRs (V2 0 0, V2 (width - 1) (height - 1)) <$> newStdGen
+  let xm = width `div` 2
+      ym = height `div` 2
+      g  = Game
+        { _snake  = (S.singleton (V2 xm ym))
+        , _food   = f
+        , _foods  = fs
+        , _score  = 0
+        , _dir    = North
+        , _dead   = False
+        , _paused = True
+        , _locked = False
+        }
+  pure $ execState nextFood g
+
+fromList :: [a] -> Stream a
+fromList = foldr (:|) (error "Streams must be infinite")
--- src/UI.hs
+++ src/UI.hs
@@ -1,136 +1,147 @@
+{-# LANGUAGE OverloadedStrings #-}
 module UI where
 
--- Imports --
 import Control.Monad (forever, void)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State.Class (put)
 import Control.Concurrent (threadDelay, forkIO)
 import Data.Maybe (fromMaybe)
 
 import Snake
 
 import Brick
-    (App(..), AttrMap, BrickEvent(..), EventM, Next, Widget,
-    customMain, neverShowCursor, continue, halt, hLimit,
-    vLimit, vBox, hBox, padRight, padLeft, padTop, padAll,
-    Padding(..), withBorderStyle, str, attrMap, withAttr,
-    emptyWidget, AttrName, on, fg, (<+>))
-
+  ( App(..), AttrMap, BrickEvent(..), EventM, Widget
+  , customMain, neverShowCursor
+  , halt
+  , hLimit, vLimit, vBox, hBox
+  , padRight, padLeft, padTop, padAll, Padding(..)
+  , withBorderStyle
+  , str
+  , attrName, attrMap, withAttr, emptyWidget, AttrName, on, fg
+  , (<+>)
+  )
 import Brick.BChan (newBChan, writeBChan)
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
+import Control.Lens ((^.))
 import qualified Graphics.Vty as V
+import qualified Graphics.Vty.CrossPlatform
+import qualified Graphics.Vty.Config
 import Data.Sequence (Seq)
 import qualified Data.Sequence as S
 import Linear.V2 (V2(..))
-import Lens.Micro ((^.))
 
+-- Types
 
--- Types --
--- Each Tick measures the passing of time
+-- | Ticks mark passing of time
+--
+-- This is our custom event that will be constantly fed into the app.
 data Tick = Tick
 
--- Named resources
+-- | Named resources
+--
+-- Not currently used, but will be easier to refactor
+-- if we call this "Name" now.
 type Name = ()
 
--- Each Cell on the grid
 data Cell = Snake | Food | Empty
 
 -- App definition
+
 app :: App Game Tick Name
-app = App { appDraw = drawUI,
-            appChooseCursor = neverShowCursor,
-            appHandleEvent = handleEvent,
-            appStartEvent = return,
-            appAttrMap = const theMap  
+app = App { appDraw = drawUI
+          , appChooseCursor = neverShowCursor
+          , appHandleEvent = handleEvent
+          , appStartEvent = pure ()
+          , appAttrMap = const theMap
           }
 
+main :: IO ()
+main = do
+  chan <- newBChan 10
+  forkIO $ forever $ do
+    writeBChan chan Tick
+    threadDelay 100000 -- decides how fast your game moves
+  g <- initGame
+  let buildVty = Graphics.Vty.CrossPlatform.mkVty Graphics.Vty.Config.defaultConfig
+  initialVty <- buildVty
+  void $ customMain initialVty buildVty (Just chan) app g
 
--- Handling events --
-handleEvent :: Game -> BrickEvent Name Tick -> EventM Name (Next Game)
-handleEvent g (AppEvent Tick)                       = continue $ step g
-handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ turn North g
-handleEvent g (VtyEvent (V.EvKey V.KDown []))       = continue $ turn South g
-handleEvent g (VtyEvent (V.EvKey V.KRight []))      = continue $ turn East g
-handleEvent g (VtyEvent (V.EvKey V.KLeft []))       = continue $ turn West g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'w') [])) = continue $ turn North g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 's') [])) = continue $ turn South g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'd') [])) = continue $ turn East g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'a') [])) = continue $ turn West g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'r') [])) = liftIO (initGame) >>= continue
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'p') [])) = halt g
-handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
-handleEvent g _                                     = continue g
+-- Handling events
 
+handleEvent :: BrickEvent Name Tick -> EventM Name Game ()
+handleEvent (AppEvent Tick)                       = step
+handleEvent (VtyEvent (V.EvKey V.KUp []))         = turn North
+handleEvent (VtyEvent (V.EvKey V.KDown []))       = turn South
+handleEvent (VtyEvent (V.EvKey V.KRight []))      = turn East
+handleEvent (VtyEvent (V.EvKey V.KLeft []))       = turn West
+handleEvent (VtyEvent (V.EvKey (V.KChar 'k') [])) = turn North
+handleEvent (VtyEvent (V.EvKey (V.KChar 'j') [])) = turn South
+handleEvent (VtyEvent (V.EvKey (V.KChar 'l') [])) = turn East
+handleEvent (VtyEvent (V.EvKey (V.KChar 'h') [])) = turn West
+handleEvent (VtyEvent (V.EvKey (V.KChar 'r') [])) = initGame >>= put
+handleEvent (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt
+handleEvent (VtyEvent (V.EvKey V.KEsc []))        = halt
+handleEvent _                                     = pure ()
 
--- Drawing --
+-- Drawing
+
 drawUI :: Game -> [Widget Name]
-drawUI g = 
-    [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g]
+drawUI g =
+  [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g ]
 
 drawStats :: Game -> Widget Name
 drawStats g = hLimit 11
-    $ vBox [ drawScore (g ^. score),
-             padTop (Pad 2) $ drawGameOver (g ^. dead)
-            ]
+  $ vBox [ drawScore (g ^. score)
+         , padTop (Pad 2) $ drawGameOver (g ^. dead)
+         ]
 
 drawScore :: Int -> Widget Name
 drawScore n = withBorderStyle BS.unicodeBold
-    $ B.borderWithLabel (str "Score")
-    $ C.hCenter
-    $ padAll 1
-    $ str $ show n
+  $ B.borderWithLabel (str "Score")
+  $ C.hCenter
+  $ padAll 1
+  $ str $ show n
 
 drawGameOver :: Bool -> Widget Name
 drawGameOver dead =
-    if dead
-        then withAttr gameOverAttr $ C.hCenter $ str "GAME OVER"
-        else emptyWidget
-
-gameOverAttr :: AttrName
-gameOverAttr = "gameOver"
+  if dead
+     then withAttr gameOverAttr $ C.hCenter $ str "GAME OVER"
+     else emptyWidget
 
 drawGrid :: Game -> Widget Name
 drawGrid g = withBorderStyle BS.unicodeBold
-    $ B.borderWithLabel (str "Snake")
-    $ vBox rows
-    where
-        rows         = [hBox $ cellsInRow r | r <- [height-1, height-2..0]]
-        cellsInRow y = [drawCoord (V2 x y) | x <- [0..width-1]]
-        drawCoord    = drawCell . cellAt
-        cellAt c
-            | c `elem` g ^. snake = Snake
-            | c == g ^. food = Food
-            | otherwise = Empty
+  $ B.borderWithLabel (str "Snake")
+  $ vBox rows
+  where
+    rows         = [hBox $ cellsInRow r | r <- [height-1,height-2..0]]
+    cellsInRow y = [drawCoord (V2 x y) | x <- [0..width-1]]
+    drawCoord    = drawCell . cellAt
+    cellAt c
+      | c `elem` g ^. snake = Snake
+      | c == g ^. food      = Food
+      | otherwise           = Empty
 
 drawCell :: Cell -> Widget Name
 drawCell Snake = withAttr snakeAttr cw
-drawCell Food = withAttr foodAttr cw
+drawCell Food  = withAttr foodAttr cw
 drawCell Empty = withAttr emptyAttr cw
 
 cw :: Widget Name
-cw = str " "
-
-snakeAttr, foodAttr, emptyAttr :: AttrName
-snakeAttr = "snakeAttr"
-foodAttr = "foodAttr"
-emptyAttr = "emptyAttr"
+cw = str "  "
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
-    [ (snakeAttr, V.blue `on` V.blue),
-      (foodAttr, V.red `on` V.red),
-      (gameOverAttr, fg V.red `V.withStyle` V.bold)
-    ]
+  [ (snakeAttr, V.blue `on` V.blue)
+  , (foodAttr, V.red `on` V.red)
+  , (gameOverAttr, fg V.red `V.withStyle` V.bold)
+  ]
 
+gameOverAttr :: AttrName
+gameOverAttr = attrName "gameOver"
 
--- Main --
-main :: IO ()
-main = do
-    chan <- newBChan 10
-    forkIO $ forever $ do
-        writeBChan chan Tick
-        threadDelay 100000  -- Decides how fast the game is
-    g <- initGame
-    void $ customMain (V.mkVty V.defaultConfig) (Just chan) app g
-
+snakeAttr, foodAttr, emptyAttr :: AttrName
+snakeAttr = attrName "snakeAttr"
+foodAttr = attrName "foodAttr"
+emptyAttr = attrName "emptyAttr"
