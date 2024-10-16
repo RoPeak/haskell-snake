90dc7f4b3bdbea8b1f325835ff00bdcc35703988
RoPeak <ronanpeacock2004@gmail.com>
2024-10-16 20:20:56 +0100

    Main UI logic

--- src/UI.hs
+++ src/UI.hs
@@ -1,1 +1,83 @@
 module UI where
+
+-- Imports --
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent (threadDelay, forkIO)
+import Data.Maybe (fromMaybe)
+
+import Snake
+
+import Brick
+    (App(..), AttrMap, BrickEvent(..), EventM, Next, Widget,
+    customMain, neverShowCursor, continue, halt, hLimit,
+    vLimit, vBox, hBox, padRight, padLeft, padTop, padAll,
+    Padding(..), withBorderStyle, str, attrMap, withAttr,
+    emptyWidget, AttrName, on, fg, (<+>))
+
+import Brick.BChan (newBChan, writeBChan)
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Center as C
+import qualified Graphics.Vty as V
+import Data.Sequence (Seq)
+import qualified Data.Sequence as S
+import Linear.V2 (V2(..))
+import Lens.Micro ((^.))
+
+
+-- Types --
+-- Each Tick measures the passing of time
+data Tick = Tick
+
+-- Named resources
+type Name = ()
+
+-- Each Cell on the grid
+data Cell = Snake | Food | Empty
+
+-- App definition
+app :: App Game Tick Name
+app = App { appDraw = drawUI,
+            appChooseCursor = neverShowCursor,
+            appHandleEvent = handleEvent,
+            appStartEvent = return,
+            appAttrMap = const theMap  
+          }
+
+
+-- Handling events --
+handleEvent :: Game -> BrickEvent Name Tick -> EventM Name (Next Game)
+handleEvent g (AppEvent Tick)                       = continue $ step g
+handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ turn North g
+handleEvent g (VtyEvent (V.EvKey V.KDown []))       = continue $ turn South g
+handleEvent g (VtyEvent (V.EvKey V.KRight []))      = continue $ turn East g
+handleEvent g (VtyEvent (V.EvKey V.KLeft []))       = continue $ turn West g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'w') [])) = continue $ turn North g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 's') [])) = continue $ turn South g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'd') [])) = continue $ turn East g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'a') [])) = continue $ turn West g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'r') [])) = liftIO (initGame) >>= continue
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'p') [])) = halt g
+handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
+handleEvent g _                                     = continue g
+
+
+-- Drawing --
+drawUI :: Game -> [Widget Name]
+drawUI = undefined
+
+theMap :: AttrMap
+theMap = undefined
+
+
+-- Main --
+main :: IO ()
+main = do
+    chan <- newBChan 10
+    forkIO $ forever $ do
+        writeBChan chan Tick
+        threadDelay 100000  -- Decides how fast the game is
+    g <- initGame
+    void $ customMain (V.mkVty V.defaultConfig) (Just chan) app g
+
